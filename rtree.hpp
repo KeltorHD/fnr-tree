42fdcf631c50f283595ad9172da8927a55b5d3f9
KeltorHD <keltorplaylife@gmail.com>
2021-03-09 23:32:56 +0300

    r-tree: add correct_tree method, fix bugs

--- rtree.hpp
+++ rtree.hpp
@@ -41,22 +41,22 @@ public:
 private:
 	struct node;
 	using node_ptr_t        = std::shared_ptr<node>;                         /*тип указателя на node*/
-	using child_array_ptr_t = std::array<node_ptr_t, max_nodes>;             /*массив указателей на потомков*/
-	using data_info_t       = std::pair<data_type, mbr_t>;                   /*полная информация об объекте*/
-	using data_array_t      = std::array<data_info_t, max_nodes>;            /*массив id на data*/
+	using child_info_t      = std::pair<node_ptr_t, mbr_t>;                  /*указатели на потомков + их mbr*/
+	using child_array_ptr_t = std::array<child_info_t, max_nodes>;             /*массив указателей на потомков*/
+	using data_info_t       = std::pair<data_type, mbr_t>;                   /*данные + mbr данных*/
+	using data_array_t      = std::array<data_info_t, max_nodes>;            /*массив id,mbr на data*/
 	using data_node_t       = std::variant<child_array_ptr_t, data_array_t>; /*указатели на потомков или массив данных*/
 
 	node_ptr_t root;
 
 	struct node
 	{
-		mbr_t mbr{};                             /*минимальный ограничивающий прямоугольник n-мерного пространства*/
+		mbr_t mbr{};                             /*минимальный ограничивающий прямоугольник всех потомков или данных в узле*/
 		size_t count_array{ 0 };                 /*количество данных или указателей в массиве*/
 		data_node_t data{ child_array_ptr_t{} }; /*если не лист, храним ptr_array_t*/
 		bool leaf{ false };                      /*лист или нет?*/
 
-		node() = default;
-		node(bool leaf)
+		node(bool leaf) /*true - листочек*/
 			: leaf(leaf)
 		{
 			if (this->leaf) this->data = data_array_t{};
@@ -65,11 +65,24 @@ private:
 	};
 
 	/*функции для работы с деревом*/
-	node_ptr_t choice_leaf(const mbr_t& mbr) const;                                       /*поиск листа, в который можно поместить новое значение*/
-	node_ptr_t node_division(node_ptr_t l1, const data_type& data, const mbr_t& mbr);     /*деление узла на 2 по квадратичному алгоритму Гуттмана*/
-	void       correct_tree(node_ptr_t n1, node_ptr_t n2);                                /*корректировка дерева*/
-	std::pair<data_info_t, data_info_t> get_first_pair(std::vector<const data_info_t&>& q);/*выбор пары из множества значений*/
-	data_info_t get_next(node_ptr_t l1, node_ptr_t l2, std::vector<const data_info_t&>& q);/*выбор следующего элемента*/
+	/*поиск листа, в который можно поместить новое значение*/
+	node_ptr_t choice_leaf(const mbr_t& mbr) const;
+
+	/*деление узла на 2 по квадратичному алгоритму Гуттмана (1984)*/
+	node_ptr_t node_division(node_ptr_t l1, const data_type& data, const mbr_t& mbr);
+
+	/*корректировка дерева*/
+	void correct_tree(node_ptr_t l1, node_ptr_t l2);                                 
+
+	/*выбор пары из множества значений*/
+	std::pair<data_info_t, data_info_t> get_first_pair(std::vector<const data_info_t&>& q);
+
+	/*выбор следующего элемента по алгоритму Гуттмана (1984)*/
+	data_info_t get_next(node_ptr_t l1, node_ptr_t l2, std::vector<const data_info_t&>& q);
+
+	/*нахождение родителя элемента*/
+	node_ptr_t get_parent(node_ptr_t l) const;                                             
+	//mbr_t      correct_mbr(node_ptr_t)
 
 	/*функции для работы с mbr*/
 	mbr_t sum_mbr(const mbr_t& m1, const mbr_t& m2) const;              /*новый mbr из 2-х*/
@@ -79,10 +92,8 @@ private:
 
 R_template
 inline R_class_area::R_tree()
-	:root(std::make_shared<node>())
+	:root(std::make_shared<node>(true))
 {
-	this->root->leaf = true;
-	this->root->data = data_array_t{};
 }
 
 R_template
@@ -97,14 +108,14 @@ inline typename R_class_area::node_ptr_t R_class_area::choice_leaf(const mbr_t& 
 		size_t need_child{};
 		for (size_t i = 0; i < tmp->count_array; i++)
 		{
-			cur_diff = this->diff_square_mbr(this->sum_mbr(ptrs[i]->mbr, mbr), mbr);
+			cur_diff = this->diff_square_mbr(this->sum_mbr(ptrs[i].second, mbr), mbr);
 			if (cur_diff <= min_diff)
 			{
 				min_diff = cur_diff;
 				need_child = i;
 			}
 		}
-		tmp = ptrs[need_child];
+		tmp = ptrs[need_child].first;
 	}
 	return tmp;
 }
@@ -127,8 +138,8 @@ inline coord_type R_class_area::diff_square_mbr(const mbr_t& m1, const mbr_t& m2
 	return std::abs(this->calc_square(m1) - this->calc_square(m2));
 }
 
-template<typename data_type, typename coord_type, size_t num_dims, typename float_type, size_t max_nodes, size_t min_nodes>
-inline coord_type R_tree<data_type, coord_type, num_dims, float_type, max_nodes, min_nodes>::calc_square(const mbr_t& m) const
+R_template
+inline coord_type R_class_area::calc_square(const mbr_t& m) const
 {
 	coord_type square{ std::abs(m[0][0] - m[1][0]) };
 	for (size_t i = 1; i < num_dims; i++)
@@ -148,8 +159,10 @@ inline void R_class_area::insert(const data_type& data, const mbr_t& mbr)
 	if (l->count_array < max_nodes) /*если значение помещается в текущий узел*/
 	{
 		data_array_t& arr{ std::get<data_array_t>(l->data) };
-		arr[l->count_array].first = data;
+		arr[l->count_array].first = data; /*добавляем значение в узел*/
 		arr[l->count_array].second = mbr;
+		l->count_array++;
+		l->mbr = l->count_array == 1 ? mbr : this->sum_mbr(l->mbr, mbr); /*если в узле 1 вершина, ставим mbr новых данных, иначе находим сумму*/
 	}
 	else /*делим узел на два*/
 	{
@@ -162,23 +175,27 @@ R_template
 inline typename R_class_area::node_ptr_t R_class_area::node_division(node_ptr_t l1, const data_type& data, const mbr_t& mbr)
 {
 	/*подготовка*/
-	node_ptr_t l2(std::make_shared<node>(true));
+	node_ptr_t l2(std::make_shared<node>(true)); /*выделение памяти на лист*/
 	data_array_t& l1_data{ std::get<data_array_t>(l1->data) };
 	data_array_t& l2_data{ std::get<data_array_t>(l2->data) };
 	std::vector<const data_info_t&> q{ l1_data.cbegin(), l1_data.cend() }; /*заполнение o*/
 	q.push_back(std::make_pair(data, mbr));
-	size_t m{ q.size() };
 	for (size_t i = 0; i < max_nodes; i++) /*очистка l1*/
 	{
 		l1_data[i].first = data_type{};
 		l1_data[i].second = mbr_t{};
 	}
 	l1->count_array = 0;
+	l1->mbr = {};
 
 	/*первые значения*/
 	std::pair<data_info_t, data_info_t> obj = this->get_first_pair(q);
 	l1_data[l1->count_array] = obj.first;
+	l1->count_array++;
+	l1->mbr = obj.first.second;  /*-_- изменяем mbr*/
 	l2_data[l2->count_array] = obj.second;
+	l2->count_array++;
+	l2->mbr = obj.second.second; /*-_- изменяем mbr*/
 
 	/*остальные значения*/
 	while (true)
@@ -191,44 +208,52 @@ inline typename R_class_area::node_ptr_t R_class_area::node_division(node_ptr_t 
 		{
 			return l2;
 		}
-		else if (m - n1 >= n)
+		else if (min_nodes - n1 >= n)
 		{
 			for (size_t i = 0; i < q.size(); i++)
 			{
 				l1_data[l1->count_array] = q[i];
 				l1->count_array++;
+				l1->mbr = this->sum_mbr(l1->mbr, q[i].second); /*изменяем mbr всего листа*/
 			}
 			return l2;
 		}
-		else if (m - n2 >= n)
+		else if (min_nodes - n2 >= n)
 		{
 			for (size_t i = 0; i < q.size(); i++)
 			{
 				l2_data[l2->count_array] = q[i];
 				l2->count_array++;
+				l2->mbr = this->sum_mbr(l2->mbr, q[i].second); /*изменяем mbr всего листа*/
 			}
 			return l2;
 		}
 
 		data_info_t o_next{ this->get_next(l1,l2,q) };
-		coord_type d1{ this->diff_square_mbr(this->sum_mbr(l1->mbr, o_next.second), o_next.second) };
-		coord_type d2{ this->diff_square_mbr(this->sum_mbr(l2->mbr, o_next.second), o_next.second) };
+		coord_type d1{ this->diff_square_mbr(this->sum_mbr(l1->mbr, o_next.second), l1->mbr) };
+		coord_type d2{ this->diff_square_mbr(this->sum_mbr(l2->mbr, o_next.second), l2->mbr) };
 
 		if (d1 < d2 || (d1 == d2 && n1 < n2))
 		{
 			l1_data[l1->count_array] = o_next;
 			l1->count_array++;
+			l1->mbr = this->sum_mbr(l1->mbr, o_next.second); /*изменяем mbr всего листа*/
 		}
 		else
 		{
 			l2_data[l2->count_array] = o_next;
 			l2->count_array++;
+			l2->mbr = this->sum_mbr(l2->mbr, o_next.second); /*изменяем mbr всего листа*/
 		}
 	}
+
+	/*корректировка дерева*/
+	this->correct_tree(l1, l2);
 }
 
 R_template
-inline std::pair<typename R_class_area::data_info_t, typename R_class_area::data_info_t> R_class_area::get_first_pair(std::vector<const data_info_t&>& q)
+inline std::pair<typename R_class_area::data_info_t, typename R_class_area::data_info_t> 
+R_class_area::get_first_pair(std::vector<const data_info_t&>& q)
 {
 	size_t o1_index{}, o2_index{};
 	coord_type max_square{ std::numeric_limits<size_t>::min() };
@@ -239,8 +264,8 @@ inline std::pair<typename R_class_area::data_info_t, typename R_class_area::data
 		{
 			if (i == j) continue;
 
-			coord_type tmp_square{ this->calc_square(this->sum_mbr(q[i].second,q[j].second)) - this->calc_square(q[i].second) - this->calc_square(q[j].second) };
-			if (tmp_square > max_square)
+			coord_type tmp_square{ this->calc_square(this->sum_mbr(q[i].second, q[j].second)) - this->calc_square(q[i].second) - this->calc_square(q[j].second) };
+			if (tmp_square >= max_square)
 			{
 				max_square = tmp_square;
 				o1_index = i;
@@ -257,7 +282,78 @@ inline std::pair<typename R_class_area::data_info_t, typename R_class_area::data
 }
 
 R_template
-inline typename R_class_area::data_info_t R_class_area::get_next(node_ptr_t l1, node_ptr_t l2, std::vector<const data_info_t&>& q)
+inline typename R_class_area::data_info_t 
+R_class_area::get_next(node_ptr_t l1, node_ptr_t l2, std::vector<const data_info_t&>& q)
 {
-	return data_info_t();
+	size_t o_index{};
+	data_info_t o{};
+	coord_type max_diff_square{ std::numeric_limits<size_t>::min() };
+	for (size_t i = 0; i < q.size(); i++)
+	{
+		coord_type d1{ this->calc_square(this->sum_mbr(l1->mbr,q[i].second)) - this->calc_square(l1->mbr) };
+		coord_type d2{ this->calc_square(this->sum_mbr(l2->mbr,q[i].second)) - this->calc_square(l2->mbr) };
+		coord_type a{ std::abs(d1 - d2) };
+
+		if (a >= max_diff_square)
+		{
+			max_diff_square = a;
+			o_index = i;
+		}
+	}
+	o = q[o_index];
+	q.erase(q.begin() + o_index);
+	return o;
+}
+
+R_template
+inline void R_class_area::correct_tree(node_ptr_t l1, node_ptr_t l2)
+{
+	node_ptr_t v1{ l1 };
+	node_ptr_t v2{ l2 };
+
+	while (true)
+	{
+		if (v1 == this->root) /*если v1 - корень*/
+		{
+			if (v2 != nullptr) /*и есть новая вершина -> произошло деление узлов*/
+			{
+				this->root.reset();
+				this->root = std::make_shared<node>(false); /*создание новой вершины*/
+
+				child_array_ptr_t& rref{ std::get<child_array_ptr_t>(this->root->data) }; /*сложим 2 листочка в корень*/
+				rref[this->root->count_array].first = v1;
+				rref[this->root->count_array].second = v1->mbr;
+				this->root->count_array++;
+				rref[this->root->count_array].first = v2;
+				rref[this->root->count_array].second = v2->mbr;
+				this->root->count_array++;
+				this->root->mbr = this->sum_mbr(v1->mbr, v2->mbr);
+			}
+			return;
+		}
+
+		/*если v1 не корень*/
+		node_ptr_t p{ this->get_parent(v1) };
+		child_array_ptr_t& rref{ std::get<child_array_ptr_t>(p->data) };
+		child_info_t& info{ *(std::find(rref.begin(), rref.end(), [&v1](const child_info_t& inf) {return inf.first == v1; })) }; /*находим запись о v1*/
+		info.second = v1->mbr; /*обновляем mbr у родителя v1*/
+		v1 = p; /*изменяем v1*/
+
+		if (v2 != nullptr)
+		{
+			if (p->count_array < max_nodes) /*если новая запись влезает*/
+			{
+				child_array_ptr_t& rref{ std::get<child_array_ptr_t>(p->data) };
+				rref[p->count_array].first = v2;
+				rref[p->count_array].second = v2->mbr;
+				p->count_array++;
+			}
+			else /*делим предка*/
+			{
+				data_info_t di{ std::get<data_info_t>(v2->data) };
+				v2.reset();
+				v2 = this->node_division(p, di.first, di.second); /*пересоздаем v2, разделяем данные в p и v2*/
+			}
+		}
+	}
 }
